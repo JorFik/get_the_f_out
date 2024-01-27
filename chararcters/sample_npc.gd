74dd4bf0978e3f83f573e871d1510c9d27d297d4
a21mr <poroshin.leonid.a@gmail.com>
2024-01-27 18:55:34 +0100

    dialogue is reusable now

--- chararcters/sample_npc.gd
+++ chararcters/sample_npc.gd
@@ -3,18 +3,8 @@ extends CharacterBody2D
 func init_dial():
 	print("DIAL")
 	Dialogic.timeline_ended.connect(_on_timeline_ended)
-	# [+] Block movement
 	get_node("/root/Start/DialogueSceneGeezer").visible = true
-	# vis of dial scene = on
-	#Draw the BG
-	#Draw the sprite and all the shit like Schranks
 	Dialogic.start("res://Resources/dialogues/timeline1.dtl")
-	#remove shit
-	#remove sprite
-	#remove bg
-	# [+] unlock movement
-
-
 
 func _on_timeline_ended():
 	Dialogic.timeline_ended.disconnect(_on_timeline_ended)
