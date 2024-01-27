775197f3fc12296243a278a908bf54f612822bdf
antonweizmann <aweizman@student.42heilbronn.de>
2024-01-27 19:48:39 +0100

    map changes

--- Scenes/Riddles/Number Riddle/input.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-signal output(text: String)
-
-func _on_line_edit_text_changed(new_text):
-	$Label.text = new_text
-	output.emit(new_text)
--- Scenes/start.gd
+++
@@ -1,6 +0,0 @@
-extends Node2D
-
-
-func _on_door_body_entered(body):
-		if body.name == "red":
-			get_tree().change_scene_to_file("res://Scenes/upper_floor.tscn")
