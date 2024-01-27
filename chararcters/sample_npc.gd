b849a9da9af1046f6e3ea283a03c4193296540ba
a21mr <poroshin.leonid.a@gmail.com>
2024-01-27 18:22:55 +0100

    snapshot

--- chararcters/sample_npc.gd
+++ chararcters/sample_npc.gd
@@ -2,12 +2,15 @@ extends CharacterBody2D
 
 func init_dial():
 	print("DIAL")
-	#Block movement
+	# [+] Block movement
+	%DialogueSceneGeezer.visible = true
+	# vis of dial scene = on
 	#Draw the BG
 	#Draw the sprite and all the shit like Schranks
 	Dialogic.start("res://Resources/dialogues/timeline1.dtl")
+	%DialogueSceneGeezer.visible = false
 	#remove shit
 	#remove sprite
 	#remove bg
-	#unlock movement
+	# [+] unlock movement
 	
