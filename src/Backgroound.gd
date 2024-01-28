181473d4099caa1f9c1505e3c8a86021a1e49632
antonweizmann <aweizman@student.42heilbronn.de>
2024-01-28 11:22:31 +0100

    start convo done

--- src/Backgroound.gd
+++ src/Backgroound.gd
@@ -1,7 +1,7 @@
 extends TextureRect
 
 func _on_start_pressed():
-	get_tree().change_scene_to_file("res://Scenes/start.tscn")
+	get_tree().change_scene_to_file("res://Scenes/first_cut_Scene.tscn")
 
 func _on_end_pressed():
 	get_tree().quit()
